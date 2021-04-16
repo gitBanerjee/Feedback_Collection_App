b2a5316356a6c9c3e76ba836a9baab30592e0d65
gitBanerjee <sourav.itsme777@gmail.com>
2021-04-16 16:22:25 +0530

    Added more info

--- index.js
+++ index.js
@@ -16,5 +16,7 @@ passport.use(new GoogleStrategy({
 app.get('/auth/google',passport.authenticate('google',{
   scope:['profile','email']
 }))
+
+app.get('/auth/google/callback',passport.authenticate('google',))
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
